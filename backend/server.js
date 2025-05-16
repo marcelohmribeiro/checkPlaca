7ad98abac26b12cbc8584577f77f4d5d898f0426
marcelohmribeiro <marcelomarquesribeiro1@gmail.com>
2025-05-16 15:32:31 -0300

    ajuste

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const { pesquisa_placa } = require('api-placa');
 const cors = require('cors');
-const app = express();
-const PORT = 3001;
+const serverless = require('serverless-http');
 
-app.use(cors())
-app.use(express.json())
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    return res.json("Bem vindo a API!")
-})
+    return res.json("Bem vindo à API!");
+});
 
-// Rota para consultar placa
 app.get("/placa/:plate", async (req, res) => {
     const plate = req.params.plate.toUpperCase();
 
     try {
         const resultado = await pesquisa_placa(plate);
-        res.json(resultado)
+        res.json(resultado);
     } catch (error) {
-        res.status(400).json({ error: 'Placa inválida ou não encontrada.' })
+        res.status(400).json({ error: 'Placa inválida ou não encontrada.' });
     }
-})
+});
 
-app.listen(PORT, () => console.log(`API rodando em http://localhost:${PORT}`))
+module.exports = app;
+module.exports.handler = serverless(app);
